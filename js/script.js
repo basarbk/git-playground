e7fb07c062117c67cf60c8f69c9631313e242094
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-26 23:23:33 +0300

    Replace mouseover script with css hover

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-document.querySelectorAll('div').forEach(item => {
-  item.onmouseover = () => {
-    item.style.color = "red"
-  }
-  item.onmouseout = () => {
-    item.style.color = "black"
-  }
-})
-
 function exportAsPng(){
   html2canvas(document.querySelector("body")).then(canvas => {
     canvas.toBlob(function(blob){
